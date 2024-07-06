53699b0dc46f142c60ca9adf9f1b1b058cbd264c
Bikash <sahb2676@gmail.com>
2024-07-07 02:41:54 +0545

    What is Array?

---
+++ DS Arrays/01-Array.js
@@ -0,0 +1,8 @@
+// An array is an ordered collection of the elements that can be accessed using a numerical index
+
+const stringArr = ["a", "b", "c", "d", "e"];
+const numArr = [1, 2, 3, 4, 5];
+const boolArr = [true, false];
+const mixed = ["1", 1, true, null, undefined, { name: "xyz" }, [1, 4, 8]];
+
+console.log(mixed);
