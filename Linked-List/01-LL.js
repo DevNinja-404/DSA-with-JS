6a28da2fcb86caff59033d7ca37eb5e0c9dbc4b6
Bikash <sahb2676@gmail.com>
2024-08-16 17:33:42 +0545

    Singly Linked List

--- Linked-List/01-LL.js
+++ Linked-List/01-LL.js
@@ -128,6 +128,52 @@ class LinkedList {
 
     return null;
   }
+
+  // the set() method is like a update method which takes two parameters (index,value) and it updates the value of that index with the passed value
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.head = value;
+      return true;
+    }
+    return false;
+  }
+
+  // The insert() method allows to insert our node to anywhere inside our linked list
+  insert(index, value) {
+    if (index === 0) {
+      return this.unshift(value);
+    } else if (index === this.length) {
+      return this.push(value);
+    }
+
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+
+    this.length++;
+
+    return true;
+  }
+
+  // The size() method is used to get the size of the current linked list
+  size() {
+    let counter = 0;
+    let temp = this.head;
+    while (temp) {
+      counter++;
+      temp = temp.next;
+    }
+    return counter;
+  }
+
+  // The clear() method allows us to clear every single element of our linked list
+  clear() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 }
 
 const myLinkedList = new LinkedList(1);
@@ -136,8 +182,11 @@ console.log("-----------------");
 
 // Testing the push method:
 myLinkedList.push(2);
-// console.log(myLinkedList);
-// console.log("-----------------");
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+console.log(myLinkedList);
+console.log("-----------------");
 
 // Testing the pop method:
 // myLinkedList.pop();
@@ -149,7 +198,7 @@ myLinkedList.push(2);
 // console.log("-----------------");
 
 // Testing the unshift method:
-myLinkedList.unshift(0);
+// myLinkedList.unshift(0);
 // console.log(myLinkedList);
 // console.log("-----------------");
 
@@ -166,8 +215,23 @@ myLinkedList.unshift(0);
 // console.log(myLinkedList.getLast());
 
 // Testing the get method:
-console.log(myLinkedList.get(0));
-console.log(myLinkedList.get(1));
-console.log(myLinkedList.get(2));
-console.log(myLinkedList.get(3));
-console.log(myLinkedList.get(4));
+// console.log(myLinkedList.get(0));
+// console.log(myLinkedList.get(1));
+// console.log(myLinkedList.get(2));
+// console.log(myLinkedList.get(3));
+// console.log(myLinkedList.get(4));
+
+// Testing the set method :
+// myLinkedList.set(1, 5);
+// console.log(myLinkedList);
+
+// Testing the insert() method :
+// console.log(myLinkedList.insert(2, 15));
+// console.log(myLinkedList.get(2));
+
+// Testing the size method :
+// console.log(myLinkedList.size());
+
+// Testing the clear method :
+myLinkedList.clear();
+console.log(myLinkedList);
